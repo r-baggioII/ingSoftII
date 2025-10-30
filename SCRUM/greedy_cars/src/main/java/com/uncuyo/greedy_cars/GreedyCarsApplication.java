7de3e11b3e5a0ef07770a132512c9cebe7024f5f
JuanLoncharich <juanandresloncha@gmail.com>
2025-10-30 13:38:34 -0300

    Estructura básica de greedy cars

---
+++ SCRUM/greedy_cars/src/main/java/com/uncuyo/greedy_cars/GreedyCarsApplication.java
@@ -0,0 +1,13 @@
+package com.uncuyo.greedy_cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreedyCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreedyCarsApplication.class, args);
+	}
+
+}
---
+++ SCRUM/greedy_cars/src/test/java/com/uncuyo/greedy_cars/GreedyCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncuyo.greedy_cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreedyCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

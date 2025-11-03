02d6d764c466e17585971a46643e14cea6d175ae
Diego Forni <71357860+diegoforni@users.noreply.github.com>
2025-11-03 15:38:50 -0300

    client initialization

---
+++ SCRUM/gredy_cars_client/gredy_cars_client/src/main/java/com/gredy_cars_client/gredy_cars_client/GredyCarsClientApplication.java
@@ -0,0 +1,13 @@
+package com.gredy_cars_client.gredy_cars_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GredyCarsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GredyCarsClientApplication.class, args);
+	}
+
+}

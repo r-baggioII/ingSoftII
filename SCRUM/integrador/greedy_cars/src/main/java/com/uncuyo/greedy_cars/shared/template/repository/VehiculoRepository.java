21caa017bb4234ae45927f5538020c6c298924ae
diegoforni <d.forni0204@gmail.com>
2025-11-07 00:11:17 +0000

    fix(vehiculos): eager load caracteristicas

--- SCRUM/integrador/greedy_cars/src/main/java/com/uncuyo/greedy_cars/shared/template/repository/VehiculoRepository.java
+++ SCRUM/integrador/greedy_cars/src/main/java/com/uncuyo/greedy_cars/shared/template/repository/VehiculoRepository.java
@@ -1,13 +1,25 @@
 package com.uncuyo.greedy_cars.shared.template.repository;
 
 
-import org.springframework.stereotype.Repository;
-import com.uncuyo.greedy_cars.shared.template.entity.Vehiculo;
-
+import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.stereotype.Repository;
+
+import com.uncuyo.greedy_cars.shared.template.entity.Vehiculo;
 
 @Repository
 public interface VehiculoRepository extends BaseRepository<Vehiculo, String> {
+
     // Método personalizado para buscar vehículo por patente
     Optional<Vehiculo> findByPatenteAndEliminadoIsFalse(String patente);
+
+    @Override
+    @EntityGraph(attributePaths = "caracteristicaVehiculo")
+    Optional<Vehiculo> findByIdAndEliminadoIsFalse(String id);
+
+    @Override
+    @EntityGraph(attributePaths = "caracteristicaVehiculo")
+    List<Vehiculo> findAllByEliminadoIsFalse();
 }
